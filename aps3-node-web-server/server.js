00fec8d33f8422da0ceb738bd1c14c525424f2ca
Diego Kremer <diego.kremer@gmail.com>
2017-10-07 20:40:12 -0300

    Adicionado novos handlers
    Adicionado novos handlers para página Sobre e Erro com JSON

--- aps3-node-web-server/server.js
+++ aps3-node-web-server/server.js
@@ -4,10 +4,30 @@ const express = require('express');
 // Cria um novo app Express
 var app = express();
 
-// Registra um handler para a request HTTP
+// Cria um handler para a request HTTP
 app.get('/', (req, res) => {
-    res.send('Olá Express')
+    //res.send('<h1>Olá Express</h1>')
+    res.send({
+        name: 'Diego',
+        likes: [
+            'Technology',
+            'Travel'
+        ]
+    });
 });
+
+// Cria um handler para página Sobre
+app.get('/sobre', (req, res) => {
+    res.send('Página Sobre')
+});
+
+// Cria um handler para página de erro com JSON
+app.get('/erro', (req, res) => {
+    res.send({
+        errorMessage:'Erro ao processar sua requisição.'
+    });
+});
+
 
 // Conecta a aplicação em uma porta da máquina
 app.listen(3000);
